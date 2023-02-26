f2cf5853e212bdcf0d37672062ab2e556212a6cc
Kleiton Utida <kleiton.utida@live.com>
2023-02-25 23:25:45 -0300

    Selecting an item

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
-import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import Relogio from "./Relogio";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+  if (selecionado?.tempo) {
+    setTempo(tempoParaSegundos(selecionado.tempo))
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 import Cronometro from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
-import style from "./App.module.scss";
 import { ITarefa } from "../types/tarefa";
+import style from "./App.module.scss";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
-      <Cronometro />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Cronometro selecionado={selecionado} />
     </div>
   );
 }
